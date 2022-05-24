6dcb3cc4aeea82b1d41966882bbf0f4f9aaa0686
Osman Pérez <operez@edutecno.com>
2022-05-24 18:20:29 -0400

    Corrección install

---
+++ src/install.js
@@ -0,0 +1,17 @@
+import HelloWorld from "./components/HelloWorld.vue";
+
+const AlertApside = {
+ install(Vue, options) {
+     console.log(options);
+  // Let's register our component globally
+  // https://vuejs.org/v2/guide/components-registration.html
+  Vue.component("hello-world", HelloWorld);
+ }
+};
+
+// Automatic installation if Vue has been added to the global scope.
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(AlertApside);
+}
+
+export default AlertApside;
