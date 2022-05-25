d101e085e2bc9d0b3341ca478321a22ef237bc0e
Osman Pérez <operez@edutecno.com>
2022-05-24 22:40:39 -0400

    Fix bug in the package

--- src/install.js
+++ src/install.js
@@ -1,23 +1,23 @@
 import HelloWorld from "./components/HelloWorld.vue";
 
-// const AlertApside = {
-//  install(Vue, options) {
-//      console.log(options);
-//   // Let's register our component globally
-//   // https://vuejs.org/v2/guide/components-registration.html
-//     Vue.component("hello-world", HelloWorld);
-//  }
-// };
+const AlertApside = {
+ install(Vue, options) {
+     console.log(options);
+  // Let's register our component globally
+  // https://vuejs.org/v2/guide/components-registration.html
+    Vue.component("hello-world-apside", HelloWorld);
+ }
+};
 
-export function install(Vue) {
-	if (install.installed) return;
-	install.installed = true;
-	Vue.component("hello-world", HelloWorld);
-}
-
-// Automatic installation if Vue has been added to the global scope.
-// if (typeof window !== 'undefined' && window.Vue) {
-//     window.Vue.use(AlertApside);
+// export function install(Vue) {
+// 	if (install.installed) return;
+// 	install.installed = true;
+// 	Vue.component("hello-world", HelloWorld);
 // }
 
-//export default Vue;
+// Automatic installation if Vue has been added to the global scope.
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(AlertApside);
+}
+
+export default AlertApside;
