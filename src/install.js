9a4097a07789ff82271ff15f73733337f359fb93
Osman Pérez <operez@edutecno.com>
2022-05-26 15:28:02 -0400

    Fix bug in the package

--- src/install.js
+++ src/install.js
@@ -1,29 +1,17 @@
 import HelloWorld from "./components/HelloWorld.vue";
-//import vuetify from '@/plugins/vuetify' // path to vuetify export
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import vuetify from '@/plugins/vuetify' // path to vuetify export
+// import Vuetify from 'vuetify'
+// import 'vuetify/dist/vuetify.min.css'
+
+console.log(Vuetify);
 
 const AlertApside = {
- install(Vue, options) {
-    console.log(options);
-    Vue.use(Vuetify)
-  // Let's register our component globally
-  // https://vuejs.org/v2/guide/components-registration.html
-    Vue.$vuetify = new Vuetify({});
-    //Vue.vuetify = new Vuetify({});
-    //Vue.prototype.$vuetify = new Vuetify({});
-    Vue.prototype.vuetify = new Vuetify({});
-    //options.$vuetify = new Vuetify({});
-    //options.vuetify = new Vuetify({});
+  install(Vue/*, options*/) {
+    Vue.use(vuetify);
+    // https://vuejs.org/v2/guide/components-registration.html
     Vue.component("hello-world-apside", HelloWorld);
- }
+  }
 };
-
-// export function install(Vue) {
-// 	if (install.installed) return;
-// 	install.installed = true;
-// 	Vue.component("hello-world", HelloWorld);
-// }
 
 // Automatic installation if Vue has been added to the global scope.
 if (typeof window !== 'undefined' && window.Vue) {
